2209f5f8caa4ca1e410c3a68bf119b4493ce9cba
Nayem Islam <nayem.islam@welldev.io>
2025-10-09 17:44:53 +0600

    initial project skeleton

---
+++ backend/core/config.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Application settings loaded from environment variables.
+    """
+
+    # Database configuration
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    # JWT settings for authentication
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    # Pydantic model configuration to load from the .env file
+    env_file_path: ClassVar[Path] = Path(__file__).parent.parent.parent / ".env"
+
+    model_config = SettingsConfigDict(
+        env_file=env_file_path, env_file_encoding="utf-8", case_sensitive=True
+    )
+
+
+# Create a single instance of the settings to be used throughout the application
+settings = Settings()
---
+++ backend/models/__init__.py
@@ -0,0 +1,4 @@
+from .chamber import Chamber
+from .doctor import Doctor
+from .doctor_chamber import DoctorChamber
+from .user import User
